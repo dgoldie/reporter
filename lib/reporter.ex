50dd5181c17963588d7c59d582c52a631d0d5dd1
Doug Goldie <dgoldie15@gmail.com>
2023-05-13 21:39:30 -0600

    init' add csv library

---
+++ lib/reporter.ex
@@ -0,0 +1,18 @@
+defmodule Reporter do
+  @moduledoc """
+  Documentation for `Reporter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Reporter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
